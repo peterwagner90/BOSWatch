0d7049c3b6541b4d4007992e104d9e4de0392a5f
Peter Wagner <Peter.Wagner@engel.at>
2024-10-24 12:38:54 +0200

    whatsapp plugin

---
+++ plugins/whatsapp/whatsapp.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+plugin to send messages to a whatsapp group
+
+@author: Peter Wagner
+
+@requires: pywhatkit
+"""
+
+#
+# Imports
+#
+import logging # Global logger
+from includes import globalVars  # Global variables
+
+# Helper function, uncomment to use
+#from includes.helper import timeHandler
+#from includes.helper import wildcardHandler
+from includes.helper import configHandler
+import pywhatkit
+##
+#
+# onLoad (init) function of plugin
+# will be called one time by the pluginLoader on start
+#
+def onLoad():
+	"""
+	While loading the plugins by pluginLoader.loadPlugins()
+	this onLoad() routine is called one time for initialize the plugin
+
+	@requires:  nothing
+
+	@return:    nothing
+	@exception: Exception if init has an fatal error so that the plugin couldn't work
+
+	"""
+	try:
+		########## User onLoad CODE ##########
+		if configHandler.checkConfig("whatsapp"):
+			logging.debug(globalVars.config.get("whatsapp", "group"))
+			pass
+		########## User onLoad CODE ##########
+	except:
+		logging.error("unknown error")
+		logging.debug("unknown error", exc_info=True)
+		raise
+
+##
+#
+# Main function of plugin
+# will be called by the alarmHandler
+#
+def run(typ,freq,data):
+	"""
+	This function is the implementation of the Plugin.
+
+	If necessary the configuration hast to be set in the config.ini.
+
+	@type    typ:  string (FMS|ZVEI|POC)
+	@param   typ:  Typ of the dataset
+	@type    data: map of data (structure see readme.md in plugin folder)
+	@param   data: Contains the parameter for dispatch
+	@type    freq: string
+	@keyword freq: frequency of the SDR Stick
+
+	@requires:  If necessary the configuration hast to be set in the config.ini.
+
+	@return:    nothing
+	@exception: nothing, make sure this function will never thrown an exception
+	"""
+	try:
+		if configHandler.checkConfig("fcm"): #read and debug the config (let empty if no config used)
+			########## User Plugin CODE ##########
+			if typ == "FMS":
+				logging.warning("%s not supported", typ)
+			elif typ == "ZVEI":
+				logging.warning("%s not supported", typ)
+			elif typ == "POC":
+				# Send a WhatsApp message
+				message = data["msg"]
+				response = pywhatkit.sendwhatmsg_to_group(globalVars.config.get("whatsapp", "group"), message)	
+				logging.info("WhatsApp Message sent %s", response)
+			else:
+				logging.warning("Invalid Typ: %s", typ)
+			########## User Plugin CODE ##########
+
+	except:
+		logging.error("unknown error")
+		logging.debug("unknown error", exc_info=True)
